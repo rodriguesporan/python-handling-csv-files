ef2afa9e011ecaf734073b791d5f73dca7ee3d4e
rodriguesporan <rodriguesporan@gmail.com>
2021-04-07 17:59:41 -0300

    chore: modified , to ;

--- __main__.py
+++ __main__.py
@@ -1,7 +1,16 @@
 import csv
 
-with open('innovators.csv', 'r') as file:
-    reader = csv.reader(file, delimiter=';')
+rows = []
+
+with open('innovators.csv', mode='r') as file:
+    reader = csv.reader(file, delimiter=',')
+
     for row in reader:
       for cell in row:
-        print(cell)
+        rows.append(cell.split(";"))
+
+with open('innovators_new.csv', mode='w') as file:
+    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for row in rows:
+      writer.writerow(row)
